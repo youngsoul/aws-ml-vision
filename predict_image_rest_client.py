5b03883d6f1f96be7dec6ddc43b848132cf8bf8e
Pat Ryan <pat.ryan@spr.com>
2019-10-30 22:07:58 -0500

    added coffee mug, added direct polly integration,

--- predict_image_rest_client.py
+++ predict_image_rest_client.py
@@ -1,7 +1,26 @@
 import requests
 import base64
 import argparse
-import subprocess
+import boto3
+import os
+
+defaultRegion = 'us-east-1'
+defaultUrl = 'https://polly.us-east-1.amazonaws.com'
+profile_name = None
+
+
+def connectToPolly(regionName=defaultRegion, endpointUrl=defaultUrl):
+    return boto3.client('polly', region_name=regionName, endpoint_url=endpointUrl)
+
+
+def speak(polly, text, format='mp3', voice='Joanna'):
+    resp = polly.synthesize_speech(OutputFormat=format, Text=text, VoiceId=voice)
+    soundfile = open('./tmp/sound.mp3', 'wb')
+    soundBytes = resp['AudioStream'].read()
+    soundfile.write(soundBytes)
+    soundfile.close()
+    os.system('afplay ./tmp/sound.mp3')  # Works only on Mac OS, sorry
+    os.remove('./tmp/sound.mp3')
 
 
 def classify_deployed(file_name):
@@ -13,21 +32,26 @@ def classify_deployed(file_name):
     return payload
 
 # Coffee,Coffee Cup,Raspberry Pie,Timmy,Water Bottle
-labels = ['Coffee', 'Coffee Cup', 'Marker', 'Raspberry Pie', 'Timmy','Water Bottle']
+labels = ['Coffee', 'Coffee Cup', 'Marker', 'Raspberry Pie', 'Timmy','Water Bottle', 'Unknown']
 voice = {
-    labels[0]: "/Users/patryan/Development/mygithub/aws-ml-vision/mp3s/speech_coffee_mug.mp3",
-    labels[1]: "/Users/patryan/Development/mygithub/aws-ml-vision/mp3s/speech_coffee_cup_recycle.mp3",
-    labels[2]: "/Users/patryan/Development/mygithub/aws-ml-vision/mp3s/speech_marker.mp3",
-    labels[3]: "/Users/patryan/Development/mygithub/aws-ml-vision/mp3s/speech_rpi.mp3",
-    labels[4]: "/Users/patryan/Development/mygithub/aws-ml-vision/mp3s/speech_timmy.mp3",
-    labels[5]: "/Users/patryan/Development/mygithub/aws-ml-vision/mp3s/speech_water_bottle.mp3"
+    labels[0]: "This is a {} mug with a probability of {}.  It is not recyclable ",
+    labels[1]: "This is a {} with a probability of {}. Please recycle this item.  ",
+    labels[2]: "This is a {} with a probability of {}.  While colorful and fun you cannot recycle them.  Dont smell them, you will leave a mark on your nose.",
+    labels[3]: "This is a {} with a probability of {}.  Possibly the coolest micro computer you can buy but you cannot recycle this.",
+    labels[4]: "This is {} the geek monkey with a probability of {}.  He can help you answer any questions, however you cannot recycle him.  That would not be right.",
+    labels[5]: "This is a {} with a probability of {}.  While you cannot recycle it, good for your for using a reusable container.",
+    labels[6]: "This item is not known to me and has a low probability. "
 }
 
-def play_audio(predicted_label):
-    print(f"Play: {predicted_label}")
-    audio_file = voice[predicted_label]
+def play_audio(predicted_label, predicted_probability):
+    prob_str = f"{int(predicted_probability*100)} percent"
 
-    return_code = subprocess.call(["afplay", audio_file])
+    print(f"Play: {predicted_label}, {predicted_probability}")
+    audio_string = voice[predicted_label]
+    audio_string = audio_string.format(predicted_label, prob_str)
+    audio_string += " Thanks for using Recycle Goo Roo"
+
+    speak(connectToPolly(), audio_string)
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
@@ -49,12 +73,20 @@ if __name__ == '__main__':
     response_json = response.json()
     status_code = response_json['statusCode']
     predicted_label = response_json['body']['predicted_label']
+    predicted_probability = response_json['body']['predicted_probability']
     print(f"Status Code: {response_json['statusCode']}")
     print(f"Predicted Label: {response_json['body']['predicted_label']}")
-    print(f"Predicted Probability: {response_json['body']['predicted_probability']}")
+    print(f"Predicted Probability: {predicted_probability}")
     print("")
     print(f"All Predictions: {response_json['body']['predictions']}")
     print("-------------------")
 
     if status_code == 200:
-        play_audio(predicted_label)
+        if predicted_probability > 0.58:
+            play_audio(predicted_label, predicted_probability)
+        else:
+            prob_str = f"{int(predicted_probability * 100)} percent"
+            speak(connectToPolly(), f"I think this is a {predicted_label} but the probability is very low at {prob_str}.  I cannot tell if you should recycle this item or not.")
+    else:
+        speak(connectToPolly(), f"DOH, something went wrong, can you try again later")
+
